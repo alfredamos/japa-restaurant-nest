369038b337890d6d7b9b47c02c7b59b41e438799
Alfred Amos <alfredamos@gmail.com>
2025-06-27 16:57:09 +0100

    add several packages to package json.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { MenuItemsModule } from './menu-items/menu-items.module';
+import { OrdersModule } from './orders/orders.module';
+import { OrderDetailsModule } from './order-details/order-details.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PrismaModule, MenuItemsModule, OrdersModule, OrderDetailsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
